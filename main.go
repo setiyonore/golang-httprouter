dff3a8510959e41e3405eee2c4ced13550f6a995
setiyonore <setiyono.ressly@gmail.com>
2022-12-01 09:55:15 +0700

    add router and params test

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		fmt.Fprint(writer, "Hello httprouter")
+	})
+	server := http.Server{Addr: "localhost:3000", Handler: router}
+	server.ListenAndServe()
+}
