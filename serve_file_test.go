49fd67aa481d856d9669e24855f61dfb5cc36403
setiyonore <setiyono.ressly@gmail.com>
2022-12-03 13:50:35 +0700

    add router pattern and serve file

---
+++ serve_file_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"embed"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+//go:embed resources
+var resources embed.FS
+
+func TestServeFile(t *testing.T) {
+	router := httprouter.New()
+	directory, _ := fs.Sub(resources, "resources")
+	router.ServeFiles("/files/*filepath", http.FS(directory))
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/files/hello.txt", nil)
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	assert.Equal(t, "Hello Http router", string(body))
+}
